6b9f52bf17a7c05abc467a9f660509e89c3c5de5
audictore <floresalonzo117@gmail.com>
2026-04-01 18:39:05 -0600

    Fix render.yaml

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+echo "Cambiando provider a postgresql..."
 sed -i 's/provider = "sqlite"/provider = "postgresql"/' prisma/schema.prisma
+echo "Instalando dependencias..."
 npm install
+echo "Generando cliente Prisma..."
 npx prisma generate
+echo "Aplicando migraciones..."
 npx prisma db push
+echo "Build completado"
